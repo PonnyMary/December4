e53106197b2258387205b86b84d98c3839a36e5f
PonnyMary <rytesstijo@gmail.com>
2019-12-04 22:37:36 +0530

    MyFirstCode

---
+++ src/gitpackage/TrialRepository.java
@@ -0,0 +1,23 @@
+package gitpackage;
+
+
+public class TrialRepository {
+	public void employeeId() {
+		System.out.println("12345");
+	}
+	public void employeeName()
+	{
+		System.out.println("Farith");
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		TrialRepository p=new TrialRepository();
+		p.employeeId();
+		p.employeeName();
+		
+	}
+
+}
